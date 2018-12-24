690dc44c349798be205c554391f8b02beec15a54
Ben <siegfriedmedia@gmail.com>
2018-12-24 15:16:46 -0800

    Dynamically generate player and bomb boards.

--- minesweeper.js
+++ minesweeper.js
@@ -1,9 +1,11 @@
-const generatePlayerboard = (numberOfRows, numberOfColums) => {
+// Generates a player's board no matter what size it is.
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   const board = [];
-
+// r = Row Index
   for (let r = 0; r < numberOfRows; r++) {
     const row = [];
-    for (let c = 0; c < numberOfColums; c++) {
+// c = Column Index
+    for (let c = 0; c < numberOfColumns; c++) {
       row.push(' ');
     }
     board.push(row);
@@ -11,4 +13,41 @@ const generatePlayerboard = (numberOfRows, numberOfColums) => {
   return board;
 };
 
-console.log(generatePlayerboard(3, 2));
+// console.log(generatePlayerBoard());
+
+// Generates a player's bomb board no matter what size it is.
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+// r = Row Index
+  for (let r = 0; r < numberOfRows; r++) {
+    const row = [];
+// c = Column Index
+    for (let c = 0; c < numberOfColumns; c++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+    board[randomRowIndex][randomColumnIndex] = 'B';
+
+    numberOfBombsPlaced++;
+  }
+  return board;
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+const playerBoard = generatePlayerBoard(3, 4);
+const bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
