96b98b44d663fea85befdddb74b8b6465ec8a9b4
Ben <siegfriedmedia@gmail.com>
2019-01-22 14:51:08 -0800

    Split up Game and Board into separate files, added export and import to the files, added babel via npm

--- board.js
+++ board.js
@@ -1,28 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
-    // If there is a bomb at the flip location, tell player they lost.
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Game over, here was the final board: ');
-      this._board.print();
-    }
-    // If there is not a bomb at the flip location, and game is not over, tell player current board.
-    else if (this._board.hasNonBombEmptySpaces()) {
-      console.log('Current Board: ');
-      this._board.print();
-    // If there is not a bomb at the flip location, and game is over, tell player they won.
-    } else {
-      console.log('Congratulations on winning! Here was your wining board: ');
-      this._board.print();
-    }
-  }
-}
-
-class Board {
+export class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfEmptySpaces = numberOfRows * numberOfColumns;
